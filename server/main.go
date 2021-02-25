0231b76eb9b0c7bd64f462d554dae8b5470b778c
idk <hankhill19580@gmail.com>
2021-02-24 20:49:33 -0500

    Adds i2p support to server

--- server/main.go
+++ server/main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net"
 	"time"
+        "github.com/eyedeekay/sam3/helper"
 )
 
 const CHECK_INTERVAL = 3 // Times BUFFER_SIZE
@@ -15,13 +16,23 @@ func main() {
 	buffersize := flag.Int("buffer", 4096, "Buffer size")
 	reportinterval := flag.Duration("report", 5*time.Second, "Report interval")
 	send := flag.Bool("send", true, "True for send, false for receive")
+        i2p  := flag.Bool("i2p", false, "Run over an I2P Streaming Service, overrides -listen")
 	flag.Parse()
 
-	ln, err := net.Listen("tcp", *listen)
-	if err != nil {
-		log.Fatalf("Could not listen on %s: %s", *listen, err)
-	}
+        var ln net.Listener
+        var err error
 
+        if *i2p {
+            ln, err = sam.I2PListener("speedtest", "127.0.0.1:7656", "speedtest")
+	    if err != nil {
+		log.Fatalf("Could not listen on %s: %s", *listen, err)
+	    }
+        } else {
+	    ln, err = net.Listen("tcp", *listen)
+	    if err != nil {
+		log.Fatalf("Could not listen on %s: %s", *listen, err)
+	    }
+        }
 	buffer := make(chan speedtest.BytesPerTime)
 	output := make(chan speedtest.BytesPerTime)
 	speedtest.SpeedMeter(buffer, output) // Speedmeter on all connections
